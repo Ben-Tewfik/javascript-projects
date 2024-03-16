115e873acd5eb15f30b040f16936b4d458ed0c61
benarba mohammed <tewfikhotspur@gmail.com>
2024-03-16 11:52:37 +0100

    add icons toggle logic

--- dark-mode/news-dark-mode/main.js
+++ dark-mode/news-dark-mode/main.js
@@ -1,4 +1,13 @@
 const toggleBtn = document.querySelector(".toggle-btn");
-toggleBtn.addEventListener("click", () => {
+toggleBtn.addEventListener("click", e => {
   document.documentElement.classList.toggle("dark-theme");
+  const moon = e.currentTarget.firstElementChild;
+  const sun = e.currentTarget.lastElementChild;
+  if (!moon.classList.contains("moon")) {
+    moon.classList.toggle("moon");
+    sun.classList.toggle("sun");
+  } else {
+    moon.classList.toggle("moon");
+    sun.classList.toggle("sun");
+  }
 });
