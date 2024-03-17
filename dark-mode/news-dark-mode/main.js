8377f607cd2f146235aff891e6596750ecf46768
benarba mohammed <tewfikhotspur@gmail.com>
2024-03-17 12:04:25 +0100

    fetch posts and display them

--- dark-mode/news-dark-mode/main.js
+++ dark-mode/news-dark-mode/main.js
@@ -1,4 +1,6 @@
 const toggleBtn = document.querySelector(".toggle-btn");
+const postsContainer = document.querySelector(".posts-container");
+const url = "https://dummyjson.com/posts";
 toggleBtn.addEventListener("click", e => {
   document.documentElement.classList.toggle("dark-theme");
   const moon = e.currentTarget.firstElementChild;
@@ -11,3 +13,28 @@ toggleBtn.addEventListener("click", e => {
     sun.classList.toggle("sun");
   }
 });
+
+async function fetchPosts() {
+  const response = await fetch(url);
+  const { posts: data } = await response.json();
+  return data;
+}
+
+function displayPosts(posts) {
+  const latestPosts = posts
+    .map(post => {
+      const { body: desc, title } = post;
+      return `<article class="single-post">
+    <h1 class="title">${title}</h1>
+    <p class="description">${desc}</p>
+  </article>`;
+    })
+    .join("");
+  postsContainer.innerHTML = latestPosts;
+}
+
+async function display() {
+  const data = await fetchPosts();
+  displayPosts(data);
+}
+display();
