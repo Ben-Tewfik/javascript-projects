20d35b67793dc34758d03a768a92bb2bc8c1dd59
benarba mohammed <tewfikhotspur@gmail.com>
2024-03-19 10:39:14 +0100

    display all products

--- filter/e-commerce-filter/filter.js
+++ filter/e-commerce-filter/filter.js
@@ -2,11 +2,30 @@ const productsContainer = document.querySelector(".products-container");
 const url = "https://dummyjson.com/products";
 
 // fetch products
-
 async function fetchProducts(url) {
   const response = await fetch(url);
   const { products } = await response.json();
-  console.log(products);
   return products;
 }
-fetchProducts(url);
+// display products
+function displayProducts(products) {
+  const allProducts = products
+    .map(product => {
+      const { title, price, thumbnail: img, images } = product;
+      return `<div class="single-product">
+    <img src=${img} alt=${title} class="img" />
+    <h2 class="product-title">${title}</h2>
+    <p class="product-price">$${price}</p>
+  </div>
+</div>`;
+    })
+    .join("");
+  productsContainer.innerHTML = allProducts;
+}
+
+async function display() {
+  const data = await fetchProducts(url);
+  displayProducts(data);
+}
+
+display();
