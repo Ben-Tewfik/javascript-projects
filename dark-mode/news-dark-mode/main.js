53b6969984aad58d9be7e997d4f046d487228cea
benarba mohammed <tewfikhotspur@gmail.com>
2024-03-22 22:08:50 +0100

    add style to the toggle dark mode btn

--- dark-mode/news-dark-mode/main.js
+++ dark-mode/news-dark-mode/main.js
@@ -1,17 +1,10 @@
 const toggleBtn = document.querySelector(".toggle-btn");
 const postsContainer = document.querySelector(".posts-container");
+const ball = document.querySelector(".ball");
 const url = "https://dummyjson.com/posts";
 toggleBtn.addEventListener("click", e => {
   document.documentElement.classList.toggle("dark-theme");
-  const moon = e.currentTarget.firstElementChild;
-  const sun = e.currentTarget.lastElementChild;
-  if (!moon.classList.contains("moon")) {
-    moon.classList.toggle("moon");
-    sun.classList.toggle("sun");
-  } else {
-    moon.classList.toggle("moon");
-    sun.classList.toggle("sun");
-  }
+  ball.classList.toggle("move");
 });
 
 async function fetchPosts() {
