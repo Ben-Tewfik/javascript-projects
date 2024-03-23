abc271ea43ea632ef6ea7f750d175d375f88cc58
benarba mohammed <tewfikhotspur@gmail.com>
2024-03-23 21:42:30 +0100

    select elements

---
+++ random user/app.js
@@ -0,0 +1,6 @@
+import get from "./utils/getElement.js";
+
+const img = get(".img");
+const title = get(".title");
+const value = get(".value");
+const btns = [...document.querySelectorAll(".icon")];
