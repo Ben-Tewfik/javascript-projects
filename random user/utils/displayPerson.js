0bf908ec7be9a1e716c89c1078c1f5b3793545a5
benarba mohammed <tewfikhotspur@gmail.com>
2024-03-25 17:32:59 +0100

    add a separate function to remove active class

--- random user/utils/displayPerson.js
+++ random user/utils/displayPerson.js
@@ -1,4 +1,5 @@
 import get from "./getElement.js";
+import toggleBtn from "./toggleBtn.js";
 const img = get(".img");
 const title = get(".title");
 const value = get(".value");
@@ -8,18 +9,14 @@ function displayUser(person) {
   title.textContent = `Hi, My name is`;
   value.textContent = person.name;
   img.src = person.image;
-  btns.forEach(btn => {
-    btn.classList.remove("active");
-  });
+  toggleBtn(btns);
   btns[0].classList.add("active");
   btns.forEach(btn => {
     btn.addEventListener("click", () => {
       const label = btn.dataset.label;
       title.textContent = `Hi, My ${label} is`;
       value.textContent = person[label];
-      btns.forEach(btn => {
-        btn.classList.remove("active");
-      });
+      toggleBtn(btns);
       btn.classList.add("active");
     });
   });
---
+++ random user/utils/toggleBtn.js
@@ -0,0 +1,6 @@
+function toggleBtn(btns) {
+  btns.forEach(btn => {
+    btn.classList.remove("active");
+  });
+}
+export default toggleBtn;
