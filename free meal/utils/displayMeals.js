03d719a157572705873e76814f2f79bd0f4b2fbe
benarba mohammed <tewfikhotspur@gmail.com>
2024-03-26 21:56:12 +0100

    change the styling of title

--- free meal/utils/displayMeals.js
+++ free meal/utils/displayMeals.js
@@ -5,6 +5,10 @@ function displayMeals(meals) {
   const newMeals = meals
     .map(meal => {
       const { idMeal: id, strMeal: title, strMealThumb: img } = meal;
+      // if (title.length > 25) {
+      //   let newTitle = title.slice(0, 25);
+      // }
+
       return `<a href="#" class="meal-link">
         <article class="meal-container">
           <img
@@ -13,7 +17,9 @@ function displayMeals(meals) {
             alt=${title}
             class="meal-img"
           />
-          <div class="meal-title">${title}</div>
+          <div class="meal-title" title=${title}>${
+        title.length > 25 ? `${title.slice(0, 22)}...` : title
+      }</div>
         </article>
       </a>`;
     })
