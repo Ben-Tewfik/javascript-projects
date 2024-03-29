ade8dfea94f395c5030c2712c552949d8b78b453
benarba mohammed <tewfikhotspur@gmail.com>
2024-03-29 18:17:25 +0100

    fetch and diplay ingredients

--- free meal/utils/displaySingleMeal.js
+++ free meal/utils/displaySingleMeal.js
@@ -3,9 +3,32 @@ import get from "./getElement.js";
 const title = get(".single-meal-title");
 const img = get(".single-meal-img");
 const description = get(".single-meal-desc");
+const ingredients = get(".single-meal-ingredients");
+
 function displaySingleMeal(meal) {
   console.log(meal);
-  const { strMeal: name, strMealThumb: image, strInstructions: desc } = meal;
+  const {
+    strMeal: name,
+    strMealThumb: image,
+    strInstructions: desc,
+    strYoutube: video,
+  } = meal;
+
+  let ingredientsArr = [];
+  for (let i = 0; i <= 20; i++) {
+    let ingredient = meal[`strIngredient${i}`];
+    let measure = meal[`strMeasure${i}`];
+    if (ingredient && measure) {
+      ingredientsArr.push(`${measure} ${ingredient}`);
+    }
+  }
+  ingredients.innerHTML = ingredientsArr
+    .map(ingredient => {
+      return `<li>
+      <i class="fa-solid fa-square-check check-icon"></i>
+      ${ingredient}</li>`;
+    })
+    .join("");
   img.src = image;
   title.textContent = name;
   description.textContent = desc;
