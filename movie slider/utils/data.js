2420bb794a3e84f4f42555e2e37d436cc921fd9e
benarba mohammed <tewfikhotspur@gmail.com>
2024-03-31 13:14:27 +0200

    create data file containing movie details

---
+++ movie slider/utils/data.js
@@ -0,0 +1,38 @@
+const movies = [
+  {
+    image: "./images/image (1).jpg",
+    title: "Dungeons & Dragons: Honour Among Thieves",
+    description:
+      "A charming thief and a band of unlikely adventurers embark on an epic quest to retrieve a long lost relic, but their charming adventure goes dangerously awry when they run afoul of the wrong people.",
+  },
+  {
+    image: "./images/image (2).jpg",
+    title: "Mission: Impossible - Dead Reckoning Part One",
+    description:
+      "Ethan Hunt and the IMF team must track down a terrifying new weapon that threatens all of humanity if it falls into the wrong hands. With control of the future and the fate of the world at stake, a deadly race around the globe begins. Confronted by a mysterious, all-powerful enemy, Ethan is forced to consider that nothing can matter more than the mission -- not even the lives of those he cares about most.",
+  },
+  {
+    image: "./images/image (3).jpg",
+    title: "Guardians of the Galaxy Vol. 3",
+    description:
+      "Still reeling from the loss of Gamora, Peter Quill must rally his team to defend the universe and protect one of their own. If the mission is not completely successful, it could possibly lead to the end of the Guardians as we know them.",
+  },
+  {
+    image: "./images/image (4).jpg",
+    title: "John Wick: Chapter 4",
+    description:
+      "With the price on his head ever increasing, legendary hit man John Wick takes his fight against the High Table global as he seeks out the most powerful players in the underworld, from New York to Paris to Japan to Berlin.",
+  },
+  {
+    image: "./images/image (5).jpg",
+    title: "Spider-Man: Across the Spider-Verse",
+    description:
+      "In an attempt to curb the Spot, a scientist, from harnessing the power of the multiverse, Miles Morales joins forces with Gwen Stacy.",
+  },
+  {
+    image: "./images/image (6).jpg",
+    title: "Ted",
+    description:
+      "By 1993, Ted the foul-mouthed teddy bear's moment of fame has passed. He's moved back in with his best friend, likable but awkward 16-year-old John Bennett, who lives in a working-class Boston home with his parents, Matty and Susan, and cousin Blaire. Matty's a blustering, blue-collar Bostonian who thinks he's the unequivocal boss of the house and doesn't like anyone challenging him, particularly his liberal-minded niece. Susan is kind, selfless and almost pathologically sweet when it comes to caring for her family. Blaire is a smart and politically correct college student who's not afraid to speak her mind and frequently clashes with her more traditionalist family.",
+  },
+];
