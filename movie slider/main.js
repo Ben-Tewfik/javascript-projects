ba86e441e80e8656a77f1f6838841d7990d72766
benarba mohammed <tewfikhotspur@gmail.com>
2024-03-31 12:40:01 +0200

    fix slideshow height and and dot style and logic

--- movie slider/main.js
+++ movie slider/main.js
@@ -2,6 +2,8 @@ import get from "./utils/getElement.js";
 
 const next = get(".next");
 const prev = get(".prev");
+const slides = [...document.querySelectorAll(".slide")];
+const dots = [...document.querySelectorAll(".dot")];
 
 let indexElement = 1;
 showSlide(indexElement);
@@ -13,13 +15,20 @@ prev.addEventListener("click", () => {
   moveSlide(-1);
 });
 
+dots.forEach((dot, i) => {
+  dot.addEventListener("click", () => {
+    changeDot(i + 1);
+  });
+});
+
 function moveSlide(n) {
   showSlide((indexElement += n));
 }
-
+function changeDot(n) {
+  showSlide((indexElement = n));
+}
 function showSlide(n) {
   console.log(n);
-  const slides = [...document.querySelectorAll(".slide")];
   if (n > slides.length) {
     indexElement = 1;
   }
@@ -29,5 +38,9 @@ function showSlide(n) {
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].classList.remove("active");
+  }
   slides[indexElement - 1].style.display = "block";
+  dots[indexElement - 1].classList.add("active");
 }
