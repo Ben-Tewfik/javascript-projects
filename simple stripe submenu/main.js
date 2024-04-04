105c1c78fa8e1c69b5cae1ca2a6231b14d30b5c0
benarba mohammed <tewfikhotspur@gmail.com>
2024-04-04 12:29:52 +0200

    import and display data in sidebar

--- simple stripe submenu/main.js
+++ simple stripe submenu/main.js
@@ -1,6 +1,8 @@
+import sublinks from "./utils/data.js";
 const closeBtn = document.querySelector(".close-btn");
 const toggleBtn = document.querySelector(".toggle-btn");
 const sidebarContainer = document.querySelector(".sidebar-container");
+const linksContainer = document.querySelector(".menu-container");
 // sidebar toggle
 toggleBtn.addEventListener("click", () => {
   sidebarContainer.classList.add("show");
@@ -8,3 +10,19 @@ toggleBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   sidebarContainer.classList.remove("show");
 });
+// display links in sidebar
+linksContainer.innerHTML = sublinks
+  .map(subLink => {
+    const { page, links } = subLink;
+    return `<article>
+  <h4 class="title">${page}</h4>
+  <div class="links-container">
+    ${links
+      .map(({ icon, url, label }) => {
+        return `<a href="${url}"><i class="${icon}"></i>${label}</a>`;
+      })
+      .join("")}
+  </div>
+</article>`;
+  })
+  .join("");
