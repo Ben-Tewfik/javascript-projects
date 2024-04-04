27730ddbd96d3e5b2b874c04f6d132f56c41838d
benarba mohammed <tewfikhotspur@gmail.com>
2024-04-04 12:11:23 +0200

    create js data array containing sidebar pages names and urls links and icons

---
+++ simple stripe submenu/utils/data.js
@@ -0,0 +1,63 @@
+const sublinks = [
+  {
+    page: "products",
+    links: [
+      {
+        icon: "fa-solid fa-credit-card",
+        url: "payment.html",
+        label: "payment",
+      },
+      {
+        icon: "fa-solid fa-money-check-dollar",
+        url: "terminal.html",
+        label: "terminal",
+      },
+      {
+        icon: "fa-solid fa-globe",
+        url: "connect.html",
+        label: "connect",
+      },
+    ],
+  },
+  {
+    page: "developers",
+    links: [
+      {
+        icon: "fa-brands fa-uncharted",
+        url: "plugins.html",
+        label: "plugins",
+      },
+      {
+        icon: "fa-solid fa-book",
+        url: "libraries.html",
+        label: "libraries",
+      },
+      {
+        icon: "fa-solid fa-chart-simple",
+        url: "api.html",
+        label: "api",
+      },
+      {
+        icon: "fa-brands fa-adversal",
+        url: "billing.html",
+        label: "billing",
+      },
+    ],
+  },
+  {
+    page: "company",
+    links: [
+      {
+        icon: "fa-solid fa-address-card",
+        url: "about.html",
+        label: "about",
+      },
+      {
+        icon: "fa-solid fa-wallet",
+        url: "customers.html",
+        label: "customers",
+      },
+    ],
+  },
+];
+export default sublinks;
