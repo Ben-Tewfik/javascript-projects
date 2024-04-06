a96db6a99bc62f8c20aaf4b875b1b944a79785aa
benarba mohammed <tewfikhotspur@gmail.com>
2024-04-06 13:03:47 +0200

    create displayFollowers function to display github followers

--- github followers pagination/main.js
+++ github followers pagination/main.js
@@ -1,10 +1,14 @@
 import get from "./utils/getElement.js";
 import fetchFollowers from "./utils/fetchFollowers.js";
 import displayFollowers from "./utils/displayFollowers.js";
+// select element
+const followersContainer = get(".followers");
+const title = get(".section-title h1");
 
 async function init() {
   const followers = await fetchFollowers();
-  displayFollowers(followers);
+  displayFollowers(followers, followersContainer);
+  title.textContent = "pagination";
 }
 
 window.addEventListener("load", init);
--- github followers pagination/utils/displayFollowers.js
+++ github followers pagination/utils/displayFollowers.js
@@ -1,4 +1,14 @@
-const displayFollowers = followers => {
-  console.log(followers);
+const displayFollowers = (followers, container) => {
+  const newFollowers = followers
+    .map(follower => {
+      const { avatar_url: image, html_url: url, login: name } = follower;
+      return `<div class="card">
+    <img src="${image}" alt="${name}" class="card-img" />
+    <h4>${name}</h4>
+    <a href="${url}" class="link" target="_blank">view profile</a>
+  </div>`;
+    })
+    .join("");
+  container.innerHTML = newFollowers;
 };
 export default displayFollowers;
