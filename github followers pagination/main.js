a78848a9b9be1c09cc01f2f2dcf819be2546c3df
benarba mohammed <tewfikhotspur@gmail.com>
2024-04-06 22:56:35 +0200

    create a paginate function to split pages

--- github followers pagination/main.js
+++ github followers pagination/main.js
@@ -1,6 +1,7 @@
 import get from "./utils/getElement.js";
 import fetchFollowers from "./utils/fetchFollowers.js";
 import displayFollowers from "./utils/displayFollowers.js";
+import paginate from "./utils/paginate.js";
 // select element
 const followersContainer = get(".followers");
 const title = get(".section-title h1");
@@ -9,6 +10,7 @@ async function init() {
   const followers = await fetchFollowers();
   displayFollowers(followers, followersContainer);
   title.textContent = "pagination";
+  const pages = paginate(followers);
 }
 
 window.addEventListener("load", init);
