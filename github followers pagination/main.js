1409dad293e52fde02b455f8160dfce5d6a8394e
benarba mohammed <tewfikhotspur@gmail.com>
2024-04-06 23:08:50 +0200

    create a function to display all buttons

--- github followers pagination/main.js
+++ github followers pagination/main.js
@@ -2,15 +2,25 @@ import get from "./utils/getElement.js";
 import fetchFollowers from "./utils/fetchFollowers.js";
 import displayFollowers from "./utils/displayFollowers.js";
 import paginate from "./utils/paginate.js";
+import displayButtons from "./utils/displayButtons.js";
 // select element
 const followersContainer = get(".followers");
 const title = get(".section-title h1");
+const btnContainer = get(".btn-container");
+
+let index = 0;
+let pages = [];
+
+function setupUi() {
+  displayFollowers(pages[index], followersContainer);
+  displayButtons(btnContainer, pages, index);
+}
 
 async function init() {
   const followers = await fetchFollowers();
-  displayFollowers(followers, followersContainer);
   title.textContent = "pagination";
-  const pages = paginate(followers);
+  pages = paginate(followers);
+  setupUi();
 }
 
 window.addEventListener("load", init);
---
+++ github followers pagination/utils/displayButtons.js
@@ -0,0 +1,12 @@
+function displayButtons(container, pages, activeIndex) {
+  const buttons = pages.map((_, pageIndex) => {
+    return `<button class='page-btn ${
+      activeIndex === pageIndex ? "active" : "null"
+    }' data-index='${pageIndex}' >${pageIndex + 1}</button>`;
+  });
+
+  buttons.push(`<button class='next-btn'>next</button>`);
+  buttons.unshift(`<button class='prev-btn'>prev</button>`);
+  container.innerHTML = buttons.join("");
+}
+export default displayButtons;
