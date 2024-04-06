25c06f6e2b1346aae81ad9640e2fec8801714728
benarba mohammed <tewfikhotspur@gmail.com>
2024-04-06 23:56:33 +0200

    add pagination click functionality

--- github followers pagination/main.js
+++ github followers pagination/main.js
@@ -23,4 +23,27 @@ async function init() {
   setupUi();
 }
 
+btnContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("btn-container")) return;
+
+  const newIndex = parseInt(e.target.dataset.index);
+  if (e.target.classList.contains("page-btn")) {
+    index = newIndex;
+  }
+
+  if (e.target.classList.contains("next-btn")) {
+    index++;
+    if (index > pages.length - 1) {
+      index = 0;
+    }
+  }
+  if (e.target.classList.contains("prev-btn")) {
+    index--;
+    if (index < 0) {
+      index = pages.length - 1;
+    }
+  }
+  setupUi();
+});
+
 window.addEventListener("load", init);
