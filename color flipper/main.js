db3aac107bbfae1f9b8a84722997e0998b7b7c18
benarba mohammed <tewfikhotspur@gmail.com>
2024-04-22 12:43:42 +0100

    add do while loop to avoid repetitive random number

--- color flipper/main.js
+++ color flipper/main.js
@@ -1,9 +1,13 @@
 const colors = ["#403233", "#f19a3e", "#98d9c2"];
 const btn = document.querySelector(".btn");
 const color = document.querySelector(".color");
-
+let lastRandomNumber;
 btn.addEventListener("click", () => {
-  const randomNumber = Math.floor(Math.random() * colors.length);
+  let randomNumber;
+  do {
+    randomNumber = Math.floor(Math.random() * colors.length);
+  } while (randomNumber === lastRandomNumber);
   document.body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
+  lastRandomNumber = randomNumber;
 });
